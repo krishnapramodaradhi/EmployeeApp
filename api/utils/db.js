42e2a1296f7447461201e0d5d287007332266cc1
Pramod <krishna.pramod2@gmail.com>
2018-01-19 19:28:01 +0530

    Removed useMongoClient as it is no longer required for mongoose 5x

--- api/utils/db.js
+++ api/utils/db.js
@@ -9,8 +9,7 @@ const mongoose          = require('mongoose');
 const logger            = require('../utils/logger');
 
 /**
- * mongoose connection string to connect to the cloud database (Mongo Atlas)
- * The object useMongoClient is set to true because of mongo clients native driver 
+ * mongoose connection string to connect to the cloud database (Mongo Atlas) 
  */
 mongoose.connect(`mongodb://`+process.env.DBUSER+`:`+process.env.DBPASS+`@cluster0-shard-00-00-kq8ud.mongodb.net:27017,cluster0-shard-00-01-kq8ud.mongodb.net:27017,cluster0-shard-00-02-kq8ud.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`);
 
